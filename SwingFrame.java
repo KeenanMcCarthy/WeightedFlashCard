f7f51f2f7296827e23c676ca1ff9080a455082e2
Keenan McCarthy <85964129+KeenanMcCarthy@users.noreply.github.com>
2024-03-31 23:43:21 -0400

    Update SwingFrame.java

--- SwingFrame.java
+++ SwingFrame.java
@@ -19,7 +19,7 @@ import javax.swing.JTextField;
 
 /* ************************************************************ *
  * Graphical User Interface build with Java Swing Framework,    *
- * Presents FlashCardDeck on desktop dashboard       			*
+ * Presents FlashCardDeck on desktop dashboard       		*
  * ************************************************************ */
 
 public class SwingFrame extends WindowAdapter implements ActionListener{
@@ -33,22 +33,22 @@ public class SwingFrame extends WindowAdapter implements ActionListener{
 
 	public void createAndShowGUI() throws IOException {
         
-		URL url = getClass().getResource("input.txt");
+	URL url = getClass().getResource("input.txt");
         File input_file = new File(url.getPath());
         this.deck.addFlashCard(input_file);
         
         this.fc = this.deck.sample();
         
-		this.button.setBounds(500, 500, 30, 30);
-		this.button.addActionListener(this);
-		this.button.setText("Enter");
+	this.button.setBounds(500, 500, 30, 30);
+	this.button.addActionListener(this);
+	this.button.setText("Enter");
 		
-		this.textField.setPreferredSize(new Dimension(100, 25));
-		this.textField.addActionListener(this);
+	this.textField.setPreferredSize(new Dimension(100, 25));
+	this.textField.addActionListener(this);
 		
-		country_label.setText(fc.country);
-		country_label.setAlignmentY(JLabel.CENTER_ALIGNMENT);
-		country_label.setHorizontalAlignment(JLabel.CENTER);
+	country_label.setText(fc.country);
+	country_label.setAlignmentY(JLabel.CENTER_ALIGNMENT);
+	country_label.setHorizontalAlignment(JLabel.CENTER);
         country_label.setForeground(new Color(0x000000));	
         country_label.setSize(500, 10);
         
@@ -67,7 +67,7 @@ public class SwingFrame extends WindowAdapter implements ActionListener{
         panel4.add(veracity_label);
         panel4.setPreferredSize(new Dimension(400, 25));
         
-		JFrame frame = new JFrame();
+	JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.setLayout(new GridLayout(4, 1));
@@ -81,30 +81,29 @@ public class SwingFrame extends WindowAdapter implements ActionListener{
  
     }
 	
-	@Override
+    @Override
     public void windowClosing(WindowEvent e)
     {
         this.deck.closeDeck();
         e.getWindow().dispose();
     }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == this.button || e.getSource() == this.textField){
-			String answer = this.textField.getText();
+    @Override
+    public void actionPerformed(ActionEvent e) {
+	if (e.getSource() == this.button || e.getSource() == this.textField){
+		String answer = this.textField.getText();
 
-			if (this.deck.isCapital(this.fc, answer)){
-				this.veracity_label.setText("correct! "+String.valueOf(fc.weight));
-			} else {
-				String text_output = "incorrect: ";
-				for (String capital: this.fc.capitals){
-					text_output += (capital + " ");
-				}
-				text_output += String.valueOf(fc.weight);
-				this.veracity_label.setText(text_output);
+		if (this.deck.isCapital(this.fc, answer)){
+			this.veracity_label.setText("correct! "+String.valueOf(fc.weight));
+		} else {
+			String text_output = "incorrect: ";
+			for (String capital: this.fc.capitals){
+				text_output += (capital + " ");
 			}
-			
-			this.textField.setText("");
+			text_output += String.valueOf(fc.weight);
+			this.veracity_label.setText(text_output);
+		}
+		        this.textField.setText("");
 			this.fc = this.deck.sample();
 			this.country_label.setText(fc.country);
 		}
